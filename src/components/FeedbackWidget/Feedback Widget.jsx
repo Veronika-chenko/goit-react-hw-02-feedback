02b292a617c47cf3b5ff5ca96d9660d080a47995
Veronika Kravchenko <aniregma@gmail.com>
2022-10-15 20:17:55 +0300

    feature/handleClick
    додає ф-цію обробки кліка

--- src/components/FeedbackWidget/Feedback Widget.jsx
+++ src/components/FeedbackWidget/Feedback Widget.jsx
@@ -1,30 +1,42 @@
-import { Component } from "react";
+import React, { Component } from "react";
 // import PropTypes from 'prop-types';
 
 export class FeedbackWidget extends Component {
+
     state = {
         good: 0,
         neutral: 0,
         bad: 0
+    };
+
+    handleClick = (evt) => {
+        console.log(1)
+        const btnName = evt.target.name;
+        this.setState((prevState) => ({
+            [btnName]: prevState[btnName] + 1
+        }))
     }
     
     render() {
+        const { good, neutral, bad } = this.state;
+        const objKeys = Object.keys(this.state);
+
         return (
             <>
                 <section style={{marginBottom: "20px"}}>
-                    <p style={{fontWeight: 700, marginBottom: "10px"}}>Please leave feedback</p>
-                    <div>
-                        <button>Good</button>
-                        <button>Neutral</button>
-                        <button>Bad</button>
-                    </div>
+                    <p style={{ fontWeight: 700, marginBottom: "10px" }}>Please leave feedback</p>
+                    
+                    {objKeys.map(key => {
+                        return <button key={key} type="button" name={key} onClick={this.handleClick}>{key}</button>
+                    })}
+
                 </section>
                 <section>
                     <p style={{fontWeight: 700, marginBottom: "10px"}}>Statistics</p>
                     <ul>
-                        <li>Good:</li>
-                        <li>Neutral:</li>
-                        <li>Bad:</li>
+                        <li>Good:{good}</li>
+                        <li>Neutral:{neutral}</li>
+                        <li>Bad:{bad}</li>
                     </ul>
                 </section>
             </>
